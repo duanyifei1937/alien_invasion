0345e1f3d580e281b1f0656b0f33f8ae82e40add
duanyifei <duanyifei1937@163.com>
2018-12-05 13:54:33 +0800

    完成左右移动

--- ship.py
+++ ship.py
@@ -17,5 +17,14 @@ class Ship:
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        self.moving_rigth = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_rigth:
+            self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
+
     def blitme(self):
         self.screen.blit(self.image, self.rect)
