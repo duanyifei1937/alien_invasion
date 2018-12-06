8c537f0f4e62ed7544dde6179cd8408aa68fd3a1
duanyifei <duanyifei1937@163.com>
2018-12-06 19:26:07 +0800

    ship speed

--- settings.py
+++ settings.py
@@ -5,7 +5,10 @@
 
 
 class Settings:
+    """a cleass to store settings for Alien Invasion."""
+
     def __init__(self):
         self.screen_width = 1200
         self.screen_heighy = 800
         self.bg_color = (230, 230, 230)
+        self.ship_speed_factor = 2
